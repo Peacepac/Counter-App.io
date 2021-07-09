936c62dae6f286d695115257d3718664a834bee6
samueluchendu <samueluchendu47@gmail.com>
2021-07-09 22:54:08 +0100

    Added github logo to link to my github repository

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ counterPlusElem.addEventListener("click", () => {
 });
 
 counterMinusElem.addEventListener("click", () => {
+
+
     count--;
     checkLimit();
     updateDisplay();
@@ -25,7 +27,7 @@ function updateDisplay() {
 
 // Added function to check number limit
 function checkLimit() {
-    if (count <= 0) {
+    if (count == 0) {
         counterMinusElem.disabled = true;
     } else if (count == 50) {
         counterPlusElem.disabled = true;
